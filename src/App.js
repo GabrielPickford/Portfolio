f6502f88477cd1190241c49f9a5dfe6c0afa210d
unknown <josegabriel.jgtc42@gmail.com>
2023-08-25 16:57:17 -0400

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react'
 
-import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers';
-import { Brand, CTA, Navbar } from './components'
+import {Navbar, Header, About, Work, Footer} from './components'
 import './App.css'
 
 const App = () => {
   return (
     <div className='App'>
-      <div className='gradient__pg'>
-        <Navbar/>
-        <Header/>
-      </div>
-      <Brand/>
-      <WhatGPT3/>
-      <Features/>
-      <Possibility/>
-      <CTA/>
-      <Blog/>
-      <Footer/>
+      <Navbar/> 
+      <Header/> 
+      <About/> 
+      <Work/> 
+      <Footer/>      
     </div>
   )
 }
